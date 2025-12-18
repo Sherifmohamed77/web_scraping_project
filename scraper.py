7ebd5682b47356ccf51e1ac025f88f114f918c16
Sherifmohamed77 <sh01015173680@gmail.com>
2025-12-19 00:10:44 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,12 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://quotes.toscrape.com/"
-response = requests.get(url)
 
-soup = BeautifulSoup(response.text, "html.parser")
+def fetch_page(url):
+    """Fetch HTML content from a given URL"""
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.text
 
-quotes = soup.find_all("span", class_="text")
 
-for q in quotes:
-    print(q.text)
+def parse_quotes(html):
+    """Extract quotes from HTML page"""
+    soup = BeautifulSoup(html, "html.parser")
+    quotes = soup.find_all("span", class_="text")
+    return [q.text for q in quotes]
+
+
+def main():
+    url = "https://quotes.toscrape.com/"
+    html = fetch_page(url)
+    quotes = parse_quotes(html)
+
+    for quote in quotes:
+        print(quote)
+
+
+if __name__ == "__main__":
+    main()
